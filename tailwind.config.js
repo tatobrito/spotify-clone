57b5bb5ccab2950c96f10b7cd623cf05bcd34aca
tatobrito <gnothisway@hotmail.com>
2024-06-21 22:21:40 -0300

    ajuste na configuração do projeto

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./pages/**/*.{html,js}", "./components/**/*.{html,js}"],
-  // ...
-};
-
-module.exports = {
   content: ["./public/index.html", "./src/**/*.{html,js}"],
-  // ...
+  theme: {
+    extend: {},
+  },
+  plugins: [],
 };
