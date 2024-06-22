9c3f74bda3c8ac48c4fd5b821544e5e625b98fed
tatobrito <gnothisway@hotmail.com>
2024-06-21 21:36:00 -0300

    new docs

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./pages/**/*.{html,js}", "./components/**/*.{html,js}"],
+  // ...
+};
+
+module.exports = {
+  content: ["./public/index.html", "./src/**/*.{html,js}"],
+  // ...
+};
