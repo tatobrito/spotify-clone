5d7b992ade4156b545966f2277fd54b45f18f252
tatobrito <gnothisway@hotmail.com>
2024-06-23 19:13:28 -0300

    att doc styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@ module.exports = {
   content: [
     "./src/**/*.{html,js}", // Adicione aqui todos os arquivos que podem conter classes do Tailwind
     "./index.html",
-    "./ProjetoTai.html",
+    "./spotifly-clone.html",
   ],
   theme: {
     extend: {},
